44bb66878171d472836cb22b2ac5cc8d3d0180e5
MartMbithi <martdevelopers254@gmail.com>
2021-09-04 10:53:15 +0300

    Implement Password Reset

--- partials/head.php
+++ partials/head.php
@@ -1,32 +1,68 @@
-<!DOCTYPE html>
-<html>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<?php
+/*
+ * Created on Sat Sep 04 2021
+ *
+ * Mart Developers Inc
+ * https://martdev.info
+ * martdevelopers254@gmail.com
+ * +254 740 847 563 / +254 737 229 776
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 Devlan Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Job Portal Management System</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+/* Load System Settings */
+$ret = "SELECT * FROM `settings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+    <!DOCTYPE html>
+    <html>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../public/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="../public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="../public/css/adminlte.min.css">
-    <!-- Google Font: Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-    <!-- IziToast CSS -->
-    <link rel="stylesheet" href="../public/plugins/iziToast/iziToast.min.css">
-    <!-- Data Tables -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css" />
-    <!-- Overlay Scroll Bars -->
-    <link rel="stylesheet" href="../public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Select2 -->
-    <link rel="stylesheet" href="../public/plugins/select2/css/select2.min.css">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo $sys->name . " | " . $sys->tagline; ?></title>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</head>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="../public/plugins/fontawesome-free/css/all.min.css">
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <!-- icheck bootstrap -->
+        <link rel="stylesheet" href="../public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="../public/css/adminlte.min.css">
+        <!-- Google Font: Source Sans Pro -->
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+        <!-- IziToast CSS -->
+        <link rel="stylesheet" href="../public/plugins/iziToast/iziToast.min.css">
+        <!-- Data Tables -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css" />
+        <!-- Overlay Scroll Bars -->
+        <link rel="stylesheet" href="../public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        <!-- Select2 -->
+        <link rel="stylesheet" href="../public/plugins/select2/css/select2.min.css">
+
+    </head>
+<?php
+} ?>
--- views/forget_password.php
+++ views/forget_password.php
@@ -29,70 +29,79 @@ session_start();
 require_once('../config/config.php');
 require_once('../config/codeGen.php');
 
-/* Login */
-if (isset($_POST['Login'])) {
-    $admin_email = $_POST['admin_email'];
-    $admin_password = sha1(md5($_POST['admin_password']));
-
-    $stmt = $mysqli->prepare("SELECT admin_email, admin_password, admin_id  FROM admin  WHERE admin_email =? AND admin_password =? ");
-    $stmt->bind_param('ss', $admin_email, $admin_password);
-    $stmt->execute(); //execute bind
-
-    $stmt->bind_result($admin_email, $admin_password, $admin_id);
-    $rs = $stmt->fetch();
-    $_SESSION['admin_id'] = $admin_id;
-
-    if ($rs) {
-        header("location:dashboard");
+/* Reset Password */
+if (isset($_POST['Reset_Password'])) {
+    //prevent posting blank value for email
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
     } else {
-        $err = "Login Failed, Please Check Your Credentials";
+        $error = 1;
+        $err = "Enter your E-mail";
+    }
+    $query = mysqli_query($mysqli, "SELECT * from `admin` WHERE admin_email='" . $admin_email . "'");
+    $num_rows = mysqli_num_rows($query);
+
+    if ($num_rows > 0) {
+        $password = $sys_gen_password; /* Find This @config/codeGen.php */
+        /* Mail User Plain Password */
+        $new_password = substr($password, 0, 10);
+        /* Hash Password  */
+        $hashed_password = sha1(md5($new_password));
+        $query = "UPDATE admin SET  admin_password=? WHERE  admin_email =?";
+        $stmt = $mysqli->prepare($query);
+        //bind paramaters
+        $rc = $stmt->bind_param('ss', $hashed_password, $email);
+        $stmt->execute();
+        /* Load Mailer */
+        require_once('../config/password_reset_mailer.php');
+        if ($stmt && $mail->send()) {
+            $success = "Password Reset Instructions Sent To Your Mail";
+        } else {
+            $err = "Password Reset Failed!, Try again $mail->ErrorInfo";
+        }
+    }
+    /* User Does Not Exist */ else {
+        $err = "Sorry, User Account With That Email Does Not Exist";
     }
 }
-
 require_once('../partials/head.php');
 ?>
 
 <body class="hold-transition login-page">
     <div class="login-box">
-
         <?php require_once('../partials/auth.php'); ?>
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your administrator session</p>
+                <p class="login-box-msg">Enter email to reset password</p>
 
                 <form method="post">
                     <div class="input-group mb-3">
-                        <input type="text" name="admin_email" required class="form-control" placeholder="Email ">
+                        <input type="text" name="email" class="form-control" placeholder="Login Username">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user-tag"></span>
                             </div>
                         </div>
                     </div>
-                    <div class="input-group mb-3">
-                        <input type="password" name="admin_password" required class="form-control" placeholder="Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
+
                     <div class="row">
-                        <div class="col-4">
-                            <div class="text-right">
-                                <button type="submit" name="Login" class="btn btn-primary btn-block">Sign In</button>
-                            </div>
+                        <div class="col-6">
+                        </div>
+                        <div class="col-6">
+                            <button type="submit" name="Reset_Password" class="btn btn-primary btn-block">Reset Password</button>
                         </div>
                     </div>
                 </form>
 
                 <p class="mb-1">
-                    <a href="forget_password">I forgot my password</a>
+                    <a href="../">I remembered my password</a>
                 </p>
+
             </div>
         </div>
     </div>
     <?php require_once('../partials/scripts.php'); ?>
+
 </body>
 
 </html>
