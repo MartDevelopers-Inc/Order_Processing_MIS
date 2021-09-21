b5749346e090c25d09aa16eab2544a83bdba58e2
Martin Mbithi <martdevelopers254@gmail.com>
2021-09-21 10:57:31 +0300

    Backend Handle On Customer Update Profile

---
+++ views/customer_profile.php
@@ -0,0 +1,177 @@
+<?php
+
+/*
+ * Created on Sun Sep 12 2021
+ *
+ * Mart Developers Inc
+ * https://martdev.info
+ * martdevelopers254@gmail.com
+ * +254 740 847 563 / +254 737 229 776
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 Devlan Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+customer_checklogin();
+$customer = $_SESSION['cus_id'];
+
+/* Update Customer Profile */
+if (isset($_POST['update_customer'])) {
+    $cus_name = $_POST['cus_name'];
+    $cus_mobile = $_POST['cus_mobile'];
+    $cus_email = $_POST['cus_email'];
+    $cus_adr = $_POST['cus_adr'];
+
+    $query = "UPDATE  customer  SET  cus_name =?, cus_mobile =?, cus_email=?, cus_password = ?, cus_adr = ?  WHERE cus_id = ? ";
+    $stmt = $mysqli->prepare($query);
+    $rc = $stmt->bind_param('ssssss', $cus_name, $cus_mobile, $cus_email, $cus_password, $cus_adr, $customer);
+    $stmt->execute();
+
+    if ($stmt) {
+        $success = "$cus_name, Profile Updated";
+    } else {
+        $info = "Please Try Again Or Try Later";
+    }
+}
+
+
+/* Update Auth Password */
+if (isset($_POST['Update_Auth'])) {
+    /* Customer Password */
+    $old_password = sha1(md5($_POST['old_password']));
+    $new_password  = sha1(md5($_POST['new_password']));
+    $confirm_password  = sha1(md5($_POST['confirm_password']));
+    /* Check If Old Passwords Match */
+    $sql = "SELECT * FROM  customer WHERE cus_id = '$customer'";
+    $res = mysqli_query($mysqli, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $row = mysqli_fetch_assoc($res);
+
+        if ($old_password != $row['admin_password']) {
+            $err =  "Please Enter Correct Old Password";
+        } elseif ($new_password != $confirm_password) {
+            $err = "Confirmation Password Does Not Match";
+        } else {
+
+            $query = "UPDATE customer SET  cus_password =? WHERE cus_id =?    ";
+            $stmt = $mysqli->prepare($query);
+            $rc = $stmt->bind_param('ss', $confirm_password, $cus_id);
+            $stmt->execute();
+            if ($stmt) {
+                $success = "Login Details Updated";
+            } else {
+                $info = "Please Try Again Or Try Later";
+            }
+        }
+    }
+}
+
+require_once('../partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('../partials/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('../partials/sidebar.php');
+        $admin_id = $_SESSION['admin_id'];
+        $ret = "SELECT * FROM admin WHERE admin_id = '$admin_id' ";
+        $stmt = $mysqli->prepare($ret);
+        $stmt->execute(); //ok
+        $res = $stmt->get_result();
+        while ($admin = $res->fetch_object()) {
+        ?>
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Profile</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                                    <li class="breadcrumb-item active">User Profile</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card card-primary card-outline">
+                                    <div class="card-body">
+                                        <form class="" method="POST">
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Email</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" required name="admin_email" value="<?php echo $admin->admin_email; ?>" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Old Password</label>
+                                                <div class="col-sm-10">
+                                                    <input type="password" required name="old_password" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">New Password</label>
+                                                <div class="col-sm-10">
+                                                    <input type="password" required name="new_password" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label class="col-sm-2 col-form-label">Confirm Password</label>
+                                                <div class="col-sm-10">
+                                                    <input type="password" required name="confirm_password" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group row ">
+                                                <div class="offset-sm-2 text-right col-sm-10">
+                                                    <button type="submit" name="Update_Auth" class="btn btn-danger">Submit</button>
+                                                </div>
+                                            </div>
+                                        </form>
+
+                                    </div><!-- /.card-body -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        <?php } ?>
+        <?php require_once('../partials/footer.php'); ?>
+    </div>
+    <!-- ./wrapper -->
+    <?php require_once('../partials/scripts.php'); ?>
+</body>
+
+
+</html>
